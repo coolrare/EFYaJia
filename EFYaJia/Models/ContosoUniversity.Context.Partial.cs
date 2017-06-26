682f7f676efbc443cdc6dd970fab9c2ccc5255f3
Will保哥 <doggy.huang@gmail.com>
2017-06-26 17:02:03 +0800

    09 練習覆寫 SaveChange() 方法，並自動寫入 ModifiedOn 屬性值

---
+++ EFYaJia/Models/ContosoUniversity.Context.Partial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFYaJia.Models
+{
+    public partial class ContosoUniversityEntities : DbContext
+    {
+        public override int SaveChanges()
+        {
+            var entries = this.ChangeTracker.Entries();
+
+            foreach (var entity in entries)
+            {
+                if (entity.Entity is Course)
+                {
+
+                }
+                if (entity.State == EntityState.Modified)
+                {
+                    entity.CurrentValues.SetValues(new { ModifiedOn = DateTime.Now });
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
